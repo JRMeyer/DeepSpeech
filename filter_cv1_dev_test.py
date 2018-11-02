8e2195152bb9dfe1228a008755fef921328f96c3
josh <jmeyer@mozilla.com>
2018-11-02 16:18:42 -0700

    kyrygz training! but theres no dev and test set, the csv files are empty

--- filter_cv1_dev_test.py
+++ filter_cv1_dev_test.py
@@ -7,12 +7,14 @@ output_folder = sys.argv[1]
 
 clips = pandas.read_csv('/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/clips.tsv', sep='\t')
 clips['path'] = clips['path'].str.replace('/', '___')
-
 locale = clips[clips['locale'] == LOCALE]
 locale_dev_paths = locale[locale['bucket'] == 'dev'].loc[:, ['path']]
 locale_test_paths = locale[locale['bucket'] == 'test'].loc[:, ['path']]
 
 mixed = pandas.read_csv('/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/cv_ky_valid.csv'.format(LOCALE))
+
+print("mixed.head():")
+print(mixed.head())
 
 locale_dev_paths['path'] = '/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/valid/'.format(LOCALE) + locale_dev_paths['path'].astype(str)
 locale_test_paths['path'] = '/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/valid/'.format(LOCALE) + locale_test_paths['path'].astype(str)
@@ -21,6 +23,17 @@ dev_indices = mixed['wav_filename'].isin(locale_dev_paths['path'])
 test_indices = mixed['wav_filename'].isin(locale_test_paths['path'])
 train_indices = ~(dev_indices | test_indices)
 
+
+mixed['wav_filename'] = '/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/'.format(LOCALE) + mixed['wav_filename'].astype(str)
+mixed['transcript'] =  mixed['transcript'].str.replace(u'\xa0', ' ')
+
+
+print("mixed[train_indices].head():")
+print(mixed[train_indices].head())
+
 mixed[dev_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_dev.csv'.format(LOCALE)), index=False)
 mixed[test_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_test.csv'.format(LOCALE)), index=False)
 mixed[train_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)), index=False)
+
+print("os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)):")
+print(os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)))
