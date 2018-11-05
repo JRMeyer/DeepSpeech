176308a5176121080fc49c7b6bf9c8b56a38c957
josh <jmeyer@mozilla.com>
2018-11-05 14:56:43 -0800

    filter-scripting

--- filter_cv1_dev_test.py
+++ filter_cv1_dev_test.py
@@ -2,38 +2,69 @@ import pandas
 import sys
 import os
 
-LOCALE = 'ky'
-output_folder = sys.argv[1]
 
-clips = pandas.read_csv('/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/clips.tsv', sep='\t')
-clips['path'] = clips['path'].str.replace('/', '___')
+'''
+
+USAGE:  $ python3 filter_cv1_dev_test.py LOCALE SAVE_TO_DIR
+ e.g.:  $ python3 filter_cv1_dev_test.py 'ky' ../keep
+
+
+The following script takes two files, clips.tsv and cv_LOCALE_valid.csv, and
+figures out which clips have been validated and belong to each "bucket", aka
+dev / test / train.
+
+Then, after we know which validated clips belong to {valid / dev / train}, 
+we save those three lists to three separate csv files.
+
+For some reason, we have .mp3 files in the clips.tsv file, and .wav files in
+the cv_LOCALE_valid.csv file.
+'''
+
+
+LOCALE = sys.argv[1]
+output_folder = sys.argv[2]
+
+#ABS_PATH='/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0'
+ABS_PATH='/home/josh/git/DeepSpeech/new'
+
+# First, we import the main csv file which stores all the data for all languages,
+# whether or not they've been validated (the file is called clips.tsv) 
+# clips.tsv ==  path	sentence    up_votes	down_votes   age     gender	accent	locale	bucket
+clips = pandas.read_csv('{}/clips.tsv'.format(ABS_PATH), sep='\t')
+# pull out data for just one language
 locale = clips[clips['locale'] == LOCALE]
-locale_dev_paths = locale[locale['bucket'] == 'dev'].loc[:, ['path']]
-locale_test_paths = locale[locale['bucket'] == 'test'].loc[:, ['path']]
 
-mixed = pandas.read_csv('/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/cv_ky_valid.csv'.format(LOCALE))
+# format file names and add abs path
+locale['path'] = locale['path'].str.replace('/', '___')
+locale['path'] = locale['path'].str.replace('mp3', 'wav')           # ugh
+dev_paths = locale[locale['bucket'] == 'dev'].loc[:, ['path']]
+test_paths = locale[locale['bucket'] == 'test'].loc[:, ['path']]
+train_paths = locale[locale['bucket'] == 'train'].loc[:, ['path']]
 
-print("mixed.head():")
-print(mixed.head())
+dev_paths['path'] = '{}/{}/valid/'.format(ABS_PATH, LOCALE) + dev_paths['path'].astype(str)
+test_paths['path'] = '{}/{}/valid/'.format(ABS_PATH, LOCALE) + test_paths['path'].astype(str)
+train_paths['path'] = '{}/{}/valid/'.format(ABS_PATH, LOCALE) + train_paths['path'].astype(str)
 
-locale_dev_paths['path'] = '/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/valid/'.format(LOCALE) + locale_dev_paths['path'].astype(str)
-locale_test_paths['path'] = '/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/valid/'.format(LOCALE) + locale_test_paths['path'].astype(str)
+# cv_LANG_valid.csv == wav_filename,wav_filesize,transcript
+validated_clips = pandas.read_csv('{}/{}/cv_{}_valid.csv'.format(ABS_PATH, LOCALE, LOCALE))
+validated_clips['wav_filename'] = '{}/{}/'.format(ABS_PATH, LOCALE) + validated_clips['wav_filename'].astype(str)
+#validated_clips['transcript'] =  validated_clips['transcript'].str.replace(u'\xa0', ' ') # for ky only?
 
-dev_indices = mixed['wav_filename'].isin(locale_dev_paths['path'])
-test_indices = mixed['wav_filename'].isin(locale_test_paths['path'])
-train_indices = ~(dev_indices | test_indices)
+# produces a single column with a Bool for whether or not the validated clip is in dev / train / test
+dev_indices = validated_clips['wav_filename'].isin(dev_paths['path'])
+test_indices = validated_clips['wav_filename'].isin(test_paths['path'])
+train_indices = validated_clips['wav_filename'].isin(train_paths['path'])
+
+print("###############################################")
+print("FILTERED CLIPS FOR THE LANGUAGE: ", str(LOCALE))
+print("Num validated clips to be used in DEV: ", validated_clips[dev_indices]['wav_filename'].count())
+print("Num validated clips to be used in TEST: ", validated_clips[test_indices]['wav_filename'].count())
+print("Num validated clips to be used in TRAIN: ", validated_clips[train_indices]['wav_filename'].count())
+print("###############################################")
 
 
-mixed['wav_filename'] = '/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0/{}/'.format(LOCALE) + mixed['wav_filename'].astype(str)
-mixed['transcript'] =  mixed['transcript'].str.replace(u'\xa0', ' ')
 
+validated_clips[dev_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_dev.csv'.format(LOCALE)), index=False)
+validated_clips[test_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_test.csv'.format(LOCALE)), index=False)
+validated_clips[train_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)), index=False)
 
-print("mixed[train_indices].head():")
-print(mixed[train_indices].head())
-
-mixed[dev_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_dev.csv'.format(LOCALE)), index=False)
-mixed[test_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_test.csv'.format(LOCALE)), index=False)
-mixed[train_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)), index=False)
-
-print("os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)):")
-print(os.path.join(output_folder, 'cv_{}_valid_train.csv'.format(LOCALE)))
